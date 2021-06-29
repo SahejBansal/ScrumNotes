77e343ca99a325c79be0f4728dcfda0d7a52125d
SahejBansal <sahej.bansal@intelligaia.com>
2021-06-29 11:09:35 +0400

    Register User API

--- src/app/controller/controller.js
+++ src/app/controller/controller.js
@@ -18,21 +18,54 @@ module.exports = {
         var saveInDBsql = 'INSERT INTO applications (name, url, client_id, client_secret) VALUES (?,?,?,?)';
         var params = [args.name, args.url, cID, cSEC];
         const result = await dbQuery(saveInDBsql, params);
-        var getSavedsql = `SELECT * FROM applications WHERE name=?`;
-        const result2 = await dbQuery(getSavedsql, [args.name]);
-        response = JSON.parse(JSON.stringify(result2))
-        vals = response[0];
+        // var getSavedsql = `SELECT * FROM applications WHERE name=?`;
+        // const result2 = await dbQuery(getSavedsql, [args.name]);
+        // response = JSON.parse(JSON.stringify(result2))
+        // vals = response[0];
         if (!result || result == null) {
             console.log(err);
         } else {
             data = {
                 cID: cID,
                 cSEC: cSEC,
-                vals
+                id: result.insertId
             }
-            console.log(vals);
             console.log("success");
             res.send(data);
+        }
+    },
+
+    async TokenGen(req, res) {
+        args = {
+            cID: req.body.cID,
+            cSEC: req.body.cSEC,
+            email: req.body.email,
+            name: req.body.name
+        }
+        var AppExistInDBsql = 'SELECT * FROM applications WHERE client_id=? and client_secret=?';
+        var params = [args.cID, args.cSEC];
+        const result = await dbQuery(AppExistInDBsql, params);
+        response = JSON.parse(JSON.stringify(result))
+        if (!result || result == null) {
+            console.log(err);
+        } else {
+            var token = randomString(21);
+            var appId = response[0].application_id;
+            var saveTokenInAppUserssql = 'INSERT INTO user_applications (application_id, token) VALUES (?,?)';
+            var params2 = [appId, token]
+            const result2 = await dbQuery(saveTokenInAppUserssql, params2);
+            if (!result2 || result2 == null) {
+                console.log(err);
+            } else {
+                
+                data = {
+                    user_id: result2.insertId,
+                    application_id: appId,
+                    token: token
+                }
+                console.log("success");
+                res.send(data);
+            }
         }
     }
 }
--- src/app/routes/router.js
+++ src/app/routes/router.js
@@ -2,4 +2,5 @@ const controller= require('../controller/controller')
 
 module.exports = function(app){
     app.post('/createClient', controller.saveInDB);
+    app.post('/createToken', controller.TokenGen);
 }
