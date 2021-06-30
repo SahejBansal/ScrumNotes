6513e97514dafae9c2977c27cf9299322cd59f7f
SahejBansal <sahej.bansal@intelligaia.com>
2021-06-30 14:49:42 +0400

    Add multiple endpoints (Untested)
    1. createGroup
    2. createPad
    3. directPad
    4. openPad (get pad URL) (working)

--- src/app/routes/router.js
+++ src/app/routes/router.js
@@ -3,4 +3,8 @@ const controller= require('../controller/controller')
 module.exports = function(app){
     app.post('/createClient', controller.saveInDB);
     app.post('/registerUser', controller.RegisterUser);
+    app.post('/createGroup', controller.createGroup);
+    app.post('/createPad', controller.createPad);
+    app.post('/directToPad', controller.directToPad);
+    app.get('/openPad', controller.getPadUrl);
 }
